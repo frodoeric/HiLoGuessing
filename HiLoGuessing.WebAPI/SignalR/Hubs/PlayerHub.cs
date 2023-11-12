fce65b3b9e9e02aa0bd812ddeec304715246fb68
Eric Costa <eric.costa@hitachivantara.com>
2023-11-12 22:15:38 +0000

    added signalr test

--- HiLoGuessing.WebAPI/SignalR/Hubs/PlayerHub.cs
+++ HiLoGuessing.WebAPI/SignalR/Hubs/PlayerHub.cs
@@ -1,13 +1,49 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using HiloGuessing.Domain.Entities;
+using Microsoft.AspNetCore.SignalR;
 
 namespace HiLoGuessing.WebAPI.SignalR.Hubs
 {
     public class PlayerHub : Hub
     {
+        public string GroupName { get; set; }
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
+
+        public async Task PlayerJoined(HiLoGuess hiLoGuess)
+        {
+            await Clients.All.SendAsync("PlayerJoined", hiLoGuess);
+        }
+
+        public async Task PlayerGuessed(HiLoGuess hiLoGuess)
+        {
+            await Clients.All.SendAsync("PlayerGuessed", hiLoGuess);
+        }
+
+        public async Task SentGuess(HiLoGuess hiLoGuess)
+        {
+            await Clients.All.SendAsync("SentGuess", hiLoGuess);
+        }
+
+        public async Task JoinGroup(string groupName)
+        {
+            GroupName = groupName;
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+            await Clients.Group(groupName).SendAsync("PlayerJoined", $"{Context.ConnectionId} joined {groupName}");
+        }
+
+        public async override Task OnDisconnectedAsync(Exception exception)
+        {
+            // Access the group name associated with the connection
+            var groupName = Context.GetHttpContext().Request.Query["group"].ToString();
+
+            // Remove the disconnected player from the group
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+
+            await base.OnDisconnectedAsync(exception);
+        }
+
     }
 
 }
